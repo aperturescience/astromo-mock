0dce92acb058ec0009d1a666db60ce909912f1d9
Gilles De Mey <gilles.de.mey@gmail.com>
2015-03-09 00:23:10 +0100

    Fixed broken app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+'use strict';
+
 var express = require('express');
-var path = require('path');
 
 var logger = require('morgan');
 var bodyParser = require('body-parser');
@@ -12,10 +13,12 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
-var cerberus = require('cerberus-middleware')({ debug: true });
+var cerberus = require('cerberus-middleware')({
+  debug: true
+});
 
 app.use(cerberus.Analytics);
 
@@ -24,9 +27,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -34,23 +37,26 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res, next) {
+
+    console.error(err);
+
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
 });
 
 
